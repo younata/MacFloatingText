92c2a3cb7bc76c1d06fdb981b22df7ab29cd7272
Rachel Brindle <you@subluminal.net>
2020-12-18 18:40:57 -0800

    Initial Commit

---
+++ TransparentTextWindow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TransparentTextWindow
+//
+//  Created by Rachel Brindle on 12/18/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
